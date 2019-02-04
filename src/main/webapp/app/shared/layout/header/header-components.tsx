1a302430cbe94d66e21f78ea45d810d3ed775a96
JustBePlayer <a.karaimin@gmail.com>
2019-02-04 14:17:51 +0200

    add homepage-carousel

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -26,9 +26,7 @@ export const BrandIcon = props => (
 
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
-    <BrandIcon />
-    <span className="brand-title">MsstPlatform</span>
-    <span className="navbar-version">{appConfig.VERSION}</span>
+    <span className="brand-title">Subtitles Translation</span>
   </NavbarBrand>
 );
 
